34b955dce26676fa500806805b4343900a993eaf
guehochoi <ibaedoli@hotmail.com>
2017-12-15 00:53:05 -0800

    Refactored POC

--- src/main/java/dupclean/DuplicateCleaner.java
+++ src/main/java/dupclean/DuplicateCleaner.java
@@ -3,6 +3,7 @@
  */
 package dupclean;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
@@ -25,50 +26,65 @@ public class DuplicateCleaner {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Path image1 = Paths.get("src/main/resources/images/1364442376228.jpg");
-		Path image2 = Paths.get("src/main/resources/images/20160204_001043.jpg");
 		
+		DuplicateCleaner duplicateCleaner = new DuplicateCleaner();
+		byte[] hash1 = duplicateCleaner.getHashInBytes("src/main/resources/images/1364442376228.jpg", "SHA-1");
+		byte[] hash2 = duplicateCleaner.getHashInBytes("src/main/resources/images/20160204_001043.jpg", "SHA-1");
+		
+
+		System.out.println("img1: " + duplicateCleaner.hashByteToString(hash1));
+		System.out.println("img2: " + duplicateCleaner.hashByteToString(hash2));
+	}
+
+	/**
+	 * function to calculate hash of a file
+	 * @param filepath path of the file to be hashed
+	 * @param algorithms desired hash algirhtm to be used
+	 * @return hash byte array of the file
+	 */
+	public byte[] getHashInBytes(String filepath, String algorithm) {
+		Path path = Paths.get(filepath);
 		MessageDigest md = null;
-		MessageDigest md2 = null;
+		InputStream is = null;
+		DigestInputStream dis = null;
+		byte[] hashInByte = new byte[20];
 		try {
-			md = MessageDigest.getInstance("SHA-1");
-			md2 = MessageDigest.getInstance("SHA-1");
-		} catch (NoSuchAlgorithmException e1) {
-			e1.printStackTrace();
-		}
-		
-		byte[] image1sha1 = new byte[20];
-		byte[] image2sha1 = new byte[20];
-		
-		try  
-		{
-			InputStream is = Files.newInputStream(image1);
-			DigestInputStream dis = new DigestInputStream(is, md);
+			md = MessageDigest.getInstance(algorithm);
+			is = Files.newInputStream(path);
+			/* Read decorated stream (dis) to EOF as normal... */
+			dis = new DigestInputStream(is, md);
 			int bytesRead = 0;
 			byte[] byteStream = new byte[1024];
 			while (dis.read(byteStream) > 0) {
 				bytesRead += byteStream.length;
 			}
-			System.out.println("bytes read: " + bytesRead);
-			image1sha1 = md.digest();
-			
-			InputStream is2 = Files.newInputStream(image2);
-			bytesRead = 0;
-			byte[] byteStream2 = new byte[1024];
-			DigestInputStream dis2 = new DigestInputStream(is2, md2);
-			while (dis2.read(byteStream2) > 0) {
-				bytesRead += byteStream2.length;
-			}
-			image2sha1 = md2.digest();
-			
-		  /* Read decorated stream (dis) to EOF as normal... */
+			System.out.println("bytes read for file " + path + ": " + bytesRead);
+			hashInByte = md.digest();
 		} catch (IOException e) {
-			System.err.print("IO Error Occurred");
 			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} finally {
+			closeQuietly(is);
+			closeQuietly(dis);
 		}
-		System.out.println("SHA1 of Image1: " + Hex.encodeHexString(image1sha1));
-		System.out.println("SHA1 of Image2: " + javax.xml.bind.DatatypeConverter.printHexBinary(image2sha1));
-		
+		return hashInByte;
 	}
-
+	protected void closeQuietly(Closeable resource) {
+		if (resource == null) 
+			return;
+		try {
+			resource.close();
+		} catch (IOException e) {
+			System.out.println("error occured while closing " + resource.toString() + ": " + e.getLocalizedMessage());
+		}
+	}
+	protected String hashByteToString(byte[] hashInByte) {
+//		Hex.encodeHexString(hashInByte) -- lowercase
+		return Hex.encodeHexString(hashInByte);
+	}
+	protected String hashByteToString(byte[] hashInByte, Object o) {
+//		javax.xml.bind.DatatypeConverter.printHexBinary(hashInByte) -- uppercase 
+		return javax.xml.bind.DatatypeConverter.printHexBinary(hashInByte);
+	}
 }
